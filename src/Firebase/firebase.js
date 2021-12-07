329dd8c1c482f9534ca18972b0a7896f4b2a4005
Admin <hari01venkat@gmail.com>
2021-12-07 19:54:51 +0530

    initial commit

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_Ttsm_LbQYHZbFgmBKBYec5gU2PF4xCY",
+  authDomain: "ppl-app-5bf09.firebaseapp.com",
+  projectId: "ppl-app-5bf09",
+  storageBucket: "ppl-app-5bf09.appspot.com",
+  messagingSenderId: "217316247950",
+  appId: "1:217316247950:web:df231fdae8102387b63cdf"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
